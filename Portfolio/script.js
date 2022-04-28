18ac55308650b1fa5fb7e60559582a6735c134f4
tallessouza <talles-swd@live.com>
2022-04-27 23:25:49 -0300

    Movimentando fontes

--- Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-function displayAbout() {
-  var icon = document.getElementById('animation')
-  icon.classList.toggle('rotate')
-  var display = document.getElementById('about')
-  display.classList.toggle('active')
-}
