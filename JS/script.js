7e226ac73324c702ff50cff2713b23df355a09e0
tallessouza <talles-swd@live.com>
2022-04-27 23:24:11 -0300

    Learrnings

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-alert('hello')
+let array = []
+for (let index = 0; index < 10; index++) {
+  array.push(index)
+}
+
+array.forEach(element => {
+  console.log(element)
+})
+
+array = [4, 2, 1, 0, 6, 3, 9, 7, 8, 5]
+
+array.sort((a, b) => a - b)
+console.log(array)
