bf78c2920ccbcd9ae3a1766edbc7e407e36e4507
tallessouza <talles-swd@live.com>
2022-07-28 17:33:43 -0300

    Promise

--- more js/script.js
+++ more js/script.js
@@ -1,13 +1,23 @@
 let users = ["Fulano", "Ciclano"]
 
-function createUser(name, callback) {
+function createUser(name) {
+  let promise = new Promise(function (resolve, reject) {
 
-  setTimeout(() => {
-    users.push(name)
-    callback()
-  }, 1000)
+    setTimeout(() => {
+      users.push(name)
+      let error = false
+      if (!error) {
+        resolve()
+      } else {
+        reject({ msg: "Erou" })
+      }
+    }, 1000)
+  })
+  return promise
 }
 
 const listUser = () => console.log(users)
 
-createUser("Beltrano", listUser)
+createUser("Beltrano").then(listUser).catch((error) => {
+  console.log(error.msg)
+})
